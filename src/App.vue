ad6a21c72f7c478d1dd4fc23d212d27c70f74f6a
Reseda <pro@pro.local>
2024-04-01 11:49:16 +0400

    add vue task-2

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <AppHeader />
     <!-- <HomePage /> -->
     <!-- <BlogPage /> -->
+    <ProductDetails />
     <BlogDetailsPage />
     <AppFooter />
   </div>
@@ -13,6 +14,7 @@ import AppHeader from './components/AppHeader.vue'
 import AppFooter from './components/AppFooter.vue'
 // import HomePage from './components/HomePage.vue'
 // import BlogPage from './components/BlogPage.vue'
+import ProductDetails from './components/ProductDetails.vue'
 import BlogDetailsPage from './components/BlogDetailsPage.vue'
 
 export default {
@@ -20,7 +22,8 @@ export default {
   components: {
     AppHeader,
     AppFooter,
-    BlogDetailsPage
+    BlogDetailsPage,
+    ProductDetails
     // HomePage
     // BlogPage
   }
